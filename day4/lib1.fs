3d261c28348b44c73b0af336c4c4105a2c644539
Nicolas Oury <Nicolas.Oury@gmail.com>
2024-02-09 19:54:46 +0000

    broken

--- day4/lib1.fs
+++ day4/lib1.fs
@@ -1,18 +1,38 @@
 VARIABLE SCORE
 0 SCORE !
 
-: in-stack ( [s] v -- [s] b )
-	depth 1 = IF 
-		DROP 0 ELSE
-		OVER OVER = ( [s] v b ) 
-		>R SWAP >R 
-		RECURSE 
- 		R> SWAP R> OR  
+VARIABLE CARDS 9 CELLS ALLOT
+ 
+CARDS 9 CELLS -1 FILL
+
+: is-winning ( value -- bool ) 
+	0 \ not winning so far (and we 
+	10 0 DO 
+		OVER CARDS I CELLS + @ = OR 
+	     LOOP
+	NIP ;
+
+
+: increase-score ( score -- new-score )
+	DUP 0= IF DROP 1 ELSE 1 lshift THEN ;
+	
+
+: count-score ( n1 n2 n3 .... nn score -- score )
+	depth 1 > IF 
+			SWAP IS-WINNING IF INCREASE-SCORE THEN 
+			RECURSE 
 		THEN ;
 
-: count-success : parse-name  ;
+: | 10 0 DO 
+  	   CARDS I CELLS + !
+         LOOP ; 
 
-: clear depth 0 do drop loop ;
+: Card  parse-name 
+   0 count-score SCORE +!  
+   ; immediate
+   
 
-: Card  parse-name clear ; immediate
-: | 13 parse ; immediate
+include input.txt
+
+0 count-score SCORE +!
+SCORE @  .
